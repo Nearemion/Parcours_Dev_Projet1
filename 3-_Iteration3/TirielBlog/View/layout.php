961d334274be67cfa56a0796a4dd055034ff6c6a
Tiriel <tiriel@free.fr>
2016-03-04 09:55:18 +0100

    Changing page title and overall jumbotron, ignoring PDOFactory second file used to keep OVH db connection informations

--- 3-_Iteration3/TirielBlog/View/layout.php
+++ 3-_Iteration3/TirielBlog/View/layout.php
@@ -5,7 +5,7 @@ $today = new \DateTime('now');
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tiriel's Home Made Blog</title>
+    <title>Ben's Home Made Blog</title>
     <meta charset="utf-8" />
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="main.css" />
@@ -13,7 +13,7 @@ $today = new \DateTime('now');
 <body>
 <div class="container">
     <header class="jumbotron">
-        <h1>Tiriel's Home Made Blog</h1>
+        <h1>Ben's Home Made Blog</h1>
         <p>Baked with love!</p>
     </header>
     <section>
